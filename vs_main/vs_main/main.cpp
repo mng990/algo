a8de92212833611bf86b05e414a48c696c9b70a0
mng990 <ryoo0504@gmail.com>
2024-04-22 04:08:52 +0900

    boj_4811(DP: top_down)

--- vs_main/vs_main/main.cpp
+++ vs_main/vs_main/main.cpp
@@ -2,70 +2,50 @@
 
 using namespace std;
 
-int T, W, ret = 0;
-int plum[1001] = { 0, };
-int DP[1001][31] = { 0, };
+int N;
+typedef long long ll;
+ll DP[62][31];
 
-int DP_TD[1001][2][31];
+// N = 1 
+// 1 -> W (DP[0][1] = 1)
+// remain: (1H, 0W) 
+// 2 -> WH (DP[1][0] = 1)
 
-void input() {
-	cin >> T >> W;
-	for (int i = 0; i < T; i++) {
-		cin >> plum[i];
-	}
+// N = 2 (DP[0][2]) 
+// 1 -> W
+// remain: (1H, 1W) 
+// 2 -> WH || WW (DP[1][1] = 2)
+// remain: (0H, 1W) || (2H, 0W) -> (DP[0][1] || DP[2][0])
+// 3 -> WHW || WWH
+// remain: (1H, 0W) || (1H, 0W) -> (DP[1][0] || DP[1][0])
+// 4 -> WHWH || WWHH => 2°³ 
 
-	memset(DP_TD, -1, sizeof(DP_TD));
-}
 
-int dfs(int idx, int tree, int cnt) {
-	if (cnt < 0) return -1e9;
-	if (idx == T) return 0;
-	int& ret = DP_TD[idx][tree][cnt];
+// DP[0][2] -> DP[1][1] -> DP[2][0] || DP[0][1]
+
+ll dfs(int h, int w) {
+	if (h < 0 || w < 0) return 0;
+	if (w == 0) return 1;
+
+	ll& ret = DP[h][w];
 	if (~ret) return ret;
-	return ret = max(dfs(idx + 1, tree ^ 1, cnt - 1), dfs(idx + 1, tree, cnt)) + (tree == plum[idx] - 1);
+	return ret = dfs(h - 1, w) + dfs(h + 1, w - 1);
 }
 
-void makeDP() {
-	for (int t = 1; t <= T; t++) {
-		for (int w = 0; w <= W; w++) {
-			if (DP[t][w] == 0) {
-				if (w > 0) DP[t][w] = max(DP[t - 1][w], DP[t][w - 1]);
-				else       DP[t][w] = DP[t - 1][w];
-			}
-
-			if ((w % 2) + 1 == plum[t - 1]) {
-				DP[t][w] = max(DP[t][w], DP[t - 1][w] + 1);
-			}
-			else if (w + 1 <= W) {
-				DP[t][w + 1] = max(DP[t][w + 1], DP[t - 1][w] + 1);
-			}
-		}
+void run() {
+	memset(DP, -1, sizeof(DP));
+	
+	while (true) {
+		cin >> N;
+		if (N == 0) break;
+		cout << dfs(0, N) << "\n";
 	}
-
-	for (int w = 0; w <= W; w++) {
-		ret = max(ret, DP[T][w]);
-	}
-}
-
-void output() {
-	cout << ret;
-}
-
-void BottomUp() {
-	input();
-	makeDP();
-	output();
-}
-
-void TopDown() {
-	input();
-	cout << max(dfs(0, 1, W - 1), dfs(0, 0, W));
 }
 
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(nullptr); cout.tie(nullptr);
-	TopDown();
+	run();
 	return 0;
 }
