4b527497ae107a5c842c6367cb2bdbf94f8d613c
mng990 <ryoo0504@gmail.com>
2024-04-22 03:12:29 +0900

    boj_2240(top_down 풀이 추가)

--- boj_2240.cpp
+++ boj_2240.cpp
@@ -6,11 +6,23 @@ int T, W, ret = 0;
 int plum[1001] = { 0, };
 int DP[1001][31] = {0,};
 
+int DP_TD[1001][2][31];
+
 void input() {
 	cin >> T >> W;
-	for (int i = 1; i <= T; i++) {
+	for (int i = 0; i < T; i++) {
 		cin >> plum[i];
 	}
+
+	memset(DP_TD, -1, sizeof(DP_TD));
+}
+
+int dfs(int idx, int tree, int cnt) {
+	if (cnt < 0) return -1e9;
+	if (idx == T) return 0;
+	int& ret = DP_TD[idx][tree][cnt];
+	if(~ret) return ret;
+	return ret = max(dfs(idx+1, tree^1, cnt-1), dfs(idx+1, tree, cnt)) + (tree == plum[idx]-1);
 }
 
 void makeDP() {
@@ -21,7 +33,7 @@ void makeDP() {
 				else       DP[t][w] = DP[t - 1][w];
 			}
 
-			if ((w % 2) + 1== plum[t]) {
+			if ((w % 2) + 1== plum[t-1]) {
 				DP[t][w] = max(DP[t][w], DP[t - 1][w] + 1);
 			}
 			else if(w+1 <= W){
@@ -39,15 +51,34 @@ void output() {
 	cout << ret;
 }
 
-void run() {
+void BottomUp() {
 	input();
 	makeDP();
 	output();
 }
 
+void TopDown() {
+	input();
+	cout << max(dfs(0,1,W-1), dfs(0,0,W));
+}
+
+void printDPTD() {
+	for (int i = 0; i < T; i++) {
+		for (int j = 0; j < 2; j++) {
+			for (int k = 0; k <= W; k++) {
+				cout << DP[i][j][k] << " ";
+			}
+			cout << "\n";
+		}
+		cout << "\n";
+	}
+	cout << "\n";
+}
+
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(nullptr); cout.tie(nullptr);
-	run();
+	TopDown();
+	//printDPTD();
 	return 0;
 }
